b0c7867a84e7809a47a13e4eb4e907384a73bffd
Ian Ramos <ianramossantos@hotmail.com>
2021-03-24 11:09:16 -0300

    fix: use try-catch on controllers to send error responses in the request

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,11 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
+    try {
+      const { user_id } = request.headers;
 
-    const users = this.listAllUsersUseCase.execute({ user_id });
+      const users = this.listAllUsersUseCase.execute({ user_id });
 
-    return response.status(200).json(users);
+      return response.status(200).json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
